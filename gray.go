64af67d41654efe33b7a1467c7e7364006d7f198
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-09-07 17:47:39 -0500

    Adds the InverseHighContrast(src image.Image) image.Image function

--- gray.go
+++ gray.go
@@ -35,6 +35,21 @@ func HighContrast(src image.Image) image.Image {
 	return dst
 }
 
+func InverseHighContrast(src image.Image) image.Image {
+	bounds := src.Bounds()
+	dst := image.NewRGBA(bounds)
+
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			pixel := inverseHighContrast(src, Point{x: x, y: y})
+
+			dst.Set(x, y, pixel)
+		}
+	}
+
+	return dst
+}
+
 func gray(img image.Image, point Point) color.Gray {
 	r, g, b, _ := img.At(point.x, point.y).RGBA()
 
@@ -47,6 +62,16 @@ func highContrast(img image.Image, point Point) color.Gray {
 	gray := gray(img, point)
 
 	if gray.Y > 128 {
+		return color.Gray{255}
+	}
+
+	return color.Gray{0}
+}
+
+func inverseHighContrast(img image.Image, point Point) color.Gray {
+	gray := gray(img, point)
+
+	if gray.Y < 128 {
 		return color.Gray{255}
 	}
 
