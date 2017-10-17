1f53a5e3b2a7731e117151d8f084b200917d9406
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-10-17 09:13:38 -0500

    Creates the NewRGBA(c color.Color) (new Color) function

--- color.go
+++ color.go
@@ -13,3 +13,15 @@ func (c *Color) RGBA() (r, g, b, a uint32) {
 
 	return
 }
+
+func NewRGBA(c color.Color) (new Color) {
+	r, g, b, a := c.RGBA()
+
+	new = Color{}
+	new.rgba.R = uint8(r >> 8)
+	new.rgba.G = uint8(g >> 8)
+	new.rgba.B = uint8(b >> 8)
+	new.rgba.A = uint8(a >> 8)
+
+	return
+}
