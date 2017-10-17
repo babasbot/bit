28f0d7f84d497c9c2d791ee77cae78b5d773daff
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-10-17 09:23:00 -0500

    Creates the (i *Image) Gray() Image function

--- grayscale.go
+++ grayscale.go
@@ -1,8 +1,24 @@
 package bit
 
 import (
+	"image"
 	"image/color"
 )
+
+func (i *Image) Gray() Image {
+	bounds := i.Bounds()
+	img := image.NewRGBA(bounds)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			rgba := NewRGBA(i.At(x, y))
+
+			img.Set(x, y, rgba.Gray())
+		}
+	}
+
+	return Image{img}
+}
 
 func (c *Color) Gray() color.Gray {
 	luma := uint8(c.Luma())
