af9ce2493d5a66ef26f23b25c2f21d8245b4ff3a
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-10-17 09:24:51 -0500

    Creates the (i *Image) And(col color.Color) Image function

--- andcolor.go
+++ andcolor.go
@@ -1,8 +1,25 @@
 package bit
 
 import (
+	"image"
 	"image/color"
 )
+
+func (i *Image) And(col color.Color) Image {
+	bounds := i.Bounds()
+	newimg := image.NewRGBA(bounds)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			c := Color{}
+			c = NewRGBA(i.At(x, y))
+
+			newimg.Set(x, y, c.And(col))
+		}
+	}
+
+	return Image{newimg}
+}
 
 func (c *Color) And(p color.Color) color.Color {
 	r, g, b, _ := p.RGBA()
