26fd13d441e7da92e723920f6810cc8a43c0586d
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-10-17 09:15:36 -0500

    Implements the image.Image interface in the Image struct

--- image.go
+++ image.go
@@ -2,8 +2,21 @@ package bit
 
 import (
 	"image"
+	"image/color"
 )
 
 type Image struct {
 	image image.Image
+}
+
+func (i *Image) ColorModel() color.Model {
+	return i.image.ColorModel()
+}
+
+func (i *Image) Bounds() image.Rectangle {
+	return i.image.Bounds()
+}
+
+func (i *Image) At(x, y int) color.Color {
+	return i.image.At(x, y)
 }
