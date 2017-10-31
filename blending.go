92adedd15c41f22959a21afe399f718e5feb59cc
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-10-31 13:27:47 -0600

    Adds the Blending(col1, col2 color.Color, alpha float32) color.Color function

---
+++ blending.go
@@ -0,0 +1,24 @@
+package bit
+
+import (
+	"image/color"
+)
+
+func Blending(col1, col2 color.Color, alpha float32) color.Color {
+	r1, g1, b1, a1 := col1.RGBA()
+	r2, g2, b2, a2 := col2.RGBA()
+
+	result := color.RGBA{}
+	result.R = uint8(alphablending(r1, r2, alpha) >> 8)
+	result.G = uint8(alphablending(g1, g2, alpha) >> 8)
+	result.B = uint8(alphablending(b1, b2, alpha) >> 8)
+	result.A = uint8(alphablending(a1, a2, alpha) >> 8)
+
+	return result
+}
+
+func alphablending(a, b uint32, alpha float32) uint32 {
+	result := float32(a)*alpha + float32(b)*(1.0-alpha)
+
+	return uint32(result)
+}
